9673c5d8a6f663a185c35d9b0c71edb8158973e4
monk-wellington <mwellington@reflexion.com>
2021-06-04 23:33:42 -0700

    Refactor Tray to subclass timer tray

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const electron = require('electron');
 
-const { app, BrowserWindow, Tray } = electron;
+const { app, BrowserWindow } = electron;
+const TimerTray = require('./app/timer-tray');
 
 let mainWindow;
 let tray;
@@ -21,23 +22,6 @@ app.on('ready', () => {
 
     const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
     const iconPath = path.join(__dirname, `/src/assets/${iconName}`);
-    tray = new Tray(iconPath);
-    tray.on('click', (event, bounds) => {
-        // click event bounds
-        const { x, y } = bounds;
-        // Window height and width
-        const { height, width } = mainWindow.getBounds();
-        if (mainWindow.isVisible()) {
-            mainWindow.hide();
-        } else {
-            mainWindow.setBounds({
-                x: x - (width / 2),
-                y,
-                height,
-                width
-            });
-            mainWindow.show();
-        }
-    });
+    tray = new TimerTray(iconPath, mainWindow);
 });
 
